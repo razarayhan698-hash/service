df7dc54ef9060066b9f58598255d3053d675abf8
razarayhan698-hash <razarayhan698@gmail.com>
2026-03-01 16:12:51 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,7 @@
-const CACHE_NAME = 'xcare-v1';
-const ASSETS = [
-  '/',
-  '/manifest.json',
-  '/logo.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('xcare-v1').then((cache) => cache.addAll(['/', '/manifest.json', '/logo.png'])));
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
