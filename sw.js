2c4935eaa90d2fc42f97c0839845fb3b8089d7e9
razarayhan698-hash <razarayhan698@gmail.com>
2026-03-01 16:01:02 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,18 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installed');
+const CACHE_NAME = 'xcare-v1';
+const ASSETS = [
+  '/',
+  '/manifest.json',
+  '/logo.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
