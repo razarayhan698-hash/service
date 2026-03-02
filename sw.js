5dd50bcdf92c73eea4a9e99c045ce00989536906
razarayhan698-hash <razarayhan698@gmail.com>
2026-03-02 16:33:34 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,32 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('xcare-v1').then((cache) => cache.addAll(['/', '/manifest.json', '/logo.png'])));
+const CACHE_NAME = 'xcare-v2'; // ভার্সন পরিবর্তন করা হয়েছে যাতে ক্যাশ ক্লিয়ার হয়
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting()) // নতুন ভার্সন সাথে সাথে একটিভ হবে
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name)) // পুরনো ক্যাশ ডিলিট করবে
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
